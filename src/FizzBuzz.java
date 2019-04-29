085e7ed7f67631b919fa09276d3e2575d96d3481
Hideya Takagi <49624989+k19gjk02@users.noreply.github.com>
2019-04-29 09:27:24 +0900

    1~50までを表示

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,10 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("1");
+	for(int count = 1;count <= 50;count++) {
+		System.out.println(count);
+	}
+
 	}
 
 }
