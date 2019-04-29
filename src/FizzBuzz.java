46d90ff344ec3473b922c21e2023e4bec410559e
Hideya Takagi <49624989+k19gjk02@users.noreply.github.com>
2019-04-29 09:46:56 +0900

    5の倍数でBuzzを表示

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,12 +6,19 @@ public class FizzBuzz {
 
 		//1から50まで表示
 		//3の倍数でFizzを表示
+		//5の倍数でBuzzを表示
+		//3と5の倍数でFizzBuzzを表示
+
 		for(int i = 1;i <= 50;i++) {
-			if((i % 3) != 0) {
-				System.out.println(i);
-			}else{
-				System.out.println("Fizz");//3の倍数でFizz
+			if((i % 3) != 0 &&(i % 5) != 0) {
+				System.out.print(i);	//3と5の倍数でなければ数字を表示
+			}if(i % 3 == 0){
+				System.out.print("Fizz");//3の倍数でFizz
 			}
+			if(i % 5 == 0){
+				System.out.print("Buzz");//5の倍数でBuzz
+			}
+			System.out.println("");//改行
 		}
 	}
 }
